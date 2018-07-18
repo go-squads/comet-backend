e5c83491d43f453d21609f43e059e0bb0d101523
reeechart <ferdinandusrichard@yahoo.co.id>
2018-07-18 14:26:47 +0700

    change order of attributes in configuration table migration up sql

--- migrations/1531897972_initialize_configuration.up.sql
+++ migrations/1531897972_initialize_configuration.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE configuration(
-  key varchar(255),
-  version integer,
   namespace_id varchar(255) FOREIGN KEY REFERENCES namespace (id),
+  version integer,
+  key varchar(255),
   value varchar(255),
   PRIMARY KEY (key, version, namespace_id)
 );
