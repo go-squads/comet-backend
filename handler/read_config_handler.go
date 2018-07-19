a662d3415b219fd051115c1393d44ca349dad45f
andriiginting <andris.ginting@gmail.com>
2018-07-19 18:30:42 +0700

    [andri/reeechart] add config fetching based on version

--- handler/read_config_handler.go
+++ handler/read_config_handler.go
@@ -10,10 +10,11 @@ import (
 func ReadConfigurationHandler(w http.ResponseWriter, r *http.Request) {
 	app := r.FormValue("app")
 	namespace := r.FormValue("namespace")
+	version := r.FormValue("version")
 
 	configurationRepo := repository.NewConfigurationRepository()
 
-	cfg := configurationRepo.GetConfiguration(app, namespace)
+	cfg := configurationRepo.GetConfiguration(app, namespace, version)
 
 	w.Header().Set("Content-type", "application/json")
 	w.WriteHeader(http.StatusOK)
--- repository/configuration.go
+++ repository/configuration.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"database/sql"
 	"log"
+	"strconv"
 
 	"github.com/go-squads/comet-backend/appcontext"
 	"github.com/go-squads/comet-backend/domain"
@@ -13,31 +14,39 @@ var err error
 type ConfigRepository struct {
 	db *sql.DB
 }
-const( 
-	getAppNameQuery = "SELECT id FROM application WHERE name=$1"
+
+const (
+	getAppNameQuery                     = "SELECT id FROM application WHERE name=$1"
 	getNamespaceIdAndActiveVersionQuery = "SELECT id,active_version FROM namespace WHERE app_id=$1 AND name=$2"
-	getConfigurationKeyValueQuery = "SELECT key,value FROM configuration WHERE version=$1 AND namespace_id=$2"
+	getConfigurationKeyValueQuery       = "SELECT key,value FROM configuration WHERE version=$1 AND namespace_id=$2"
 )
 
-func (self ConfigRepository) GetConfiguration(appName string, namespaceName string) []domain.Configuration {
+func (self ConfigRepository) GetConfiguration(appName string, namespaceName string, version string) []domain.Configuration {
 	var cfg []domain.Configuration
 	var activeVersion int
 	var applicationId int
 	var namespaceId int
+	var rows *sql.Rows
 
 	_ = self.db.QueryRow(getAppNameQuery, appName).Scan(&applicationId)
 	_ = self.db.QueryRow(getNamespaceIdAndActiveVersionQuery, applicationId, namespaceName).Scan(&namespaceId, &activeVersion)
-	row, err := self.db.Query(getConfigurationKeyValueQuery, activeVersion, namespaceId)
+
+	if version != "" {
+		versionInt, _ := strconv.Atoi(version)
+		rows, err = self.db.Query(getConfigurationKeyValueQuery, versionInt, namespaceId)
+	} else {
+		rows, err = self.db.Query(getConfigurationKeyValueQuery, activeVersion, namespaceId)
+	}
 
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
 
-	for row.Next() {
+	for rows.Next() {
 		var key string
 		var value string
 
-		err = row.Scan(&key, &value)
+		err = rows.Scan(&key, &value)
 		cfg = append(cfg, domain.Configuration{namespaceId, activeVersion, key, value})
 	}
 	return cfg
