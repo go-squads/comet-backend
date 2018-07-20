81d1c7aa46fc0dfa479d13511777cb2e814b5dc1
reeechart <ferdinandusrichard@yahoo.co.id>
2018-07-20 14:06:51 +0700

    change database schema of table namespace and configuration_change in migration package

--- migrations/1531897782_initialize_namespace.up.sql
+++ migrations/1531897782_initialize_namespace.up.sql
@@ -2,5 +2,6 @@ CREATE TABLE namespace(
   id serial PRIMARY KEY,
   name varchar(255),
   app_id FOREIGN KEY REFERENCES application (id),
-  active_version serial
+  active_version integer
+  latest_version integer
 );
--- migrations/1531898123_initialize_configuration_change.up.sql
+++ migrations/1531898123_initialize_configuration_change.up.sql
@@ -1,7 +1,6 @@
 CREATE TABLE configuration_change(
 	history_id integer REFERENCES history (id),
 	key varchar(255),
-	old_value varchar(255),
 	new_value varchar(255),
 	PRIMARY KEY (history_id, key)
 );
