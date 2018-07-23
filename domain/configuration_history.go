7d747c9e3c67eee62064388bfb7a8f2e3a4cf7fc
andriiginting <andris.ginting@gmail.com>
2018-07-23 22:03:31 +0700

    add configuration_ history file

---
+++ domain/configuration_history.go
@@ -0,0 +1,10 @@
+package domain
+
+type ConfigurationHistory struct {
+	Username           string `json:"username"`
+	Namespace          string `json:"namespace"`
+	PredecessorVersion int    `json:"predecessor_version"`
+	SuccessorVersion   int    `json:"successor_version"`
+	Key                string `json:"key"`
+	Value              string `json:"value"`
+}
