7fb957c87c41b9a75f0165f889ffee9f9a340b69
reeechart <ferdinandusrichard@yahoo.co.id>
2018-07-26 12:04:20 +0700

    add unique constraint to username, add name and salt column to users table

--- migrations/1531897914_initialize_users.up.sql
+++ migrations/1531897914_initialize_users.up.sql
@@ -1,7 +1,9 @@
 CREATE TABLE users(
 	id serial PRIMARY KEY,
-	username varchar(255),
+	username varchar(255) UNIQUE,
+	name varchar(255),
 	password varchar(255),
+	salt varchar(255),
 	role varchar(255),
 	token varchar(255)
 );
