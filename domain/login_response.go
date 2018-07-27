3672c326c25558609ece19b0b7f46b7c594b293b
andriiginting <andris.ginting@gmail.com>
2018-07-27 15:18:07 +0700

    [andri/reeechart] change login response

--- domain/login_response.go
+++ domain/login_response.go
@@ -3,5 +3,5 @@ package domain
 type LoginResponse struct {
 	Status  int    `json:"status"`
 	Message string `json:"message"`
-	Token   string `json:"token"`
+	Token   string `json:"token,omitempty"`
 }
