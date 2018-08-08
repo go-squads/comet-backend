58f33a187e4822f28b352d2f811350dcd02bc85c
Andri Suranta Ginting <andris.ginting@gmail.com>
2018-08-08 14:51:23 +0700

    change login response

--- domain/login_response.go
+++ domain/login_response.go
@@ -1,7 +1,9 @@
 package domain
 
 type LoginResponse struct {
-	Status  int    `json:"status"`
-	Message string `json:"message"`
-	Token   string `json:"token,omitempty"`
+	Status    int    `json:"status"`
+	Fullname  string `json:"full_name,omitempty"`
+	RoleBased string `json:"user_role,omitempty"`
+	Message   string `json:"message"`
+	Token     string `json:"token,omitempty"`
 }
