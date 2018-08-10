2b80d72d16313e36463780fccebf99892d5b7770
Andri Suranta Ginting <andris.ginting@gmail.com>
2018-08-10 23:26:27 +0700

    change endpoint route

--- server/router.go
+++ server/router.go
@@ -13,8 +13,8 @@ func Router() *mux.Router {
 	router.HandleFunc("/configuration/{app}/{namespace}", WithAuth(handler.ReadConfigurationHandler)).Methods("GET")
 	router.HandleFunc("/configuration/history/{app}/{namespace}", WithAuth(handler.ReadHistoryConfiguration)).Methods("GET")
 	router.HandleFunc("/application", WithAuth(handler.GetListOfApplication)).Methods("GET")
-	router.HandleFunc("/application/create", WithAuth(handler.InsertNewApplication)).Methods("POST")
-	router.HandleFunc("/application/create/{appName}", handler.InsertNewNamespace).Methods("POST")
+	router.HandleFunc("/application", WithAuth(handler.InsertNewApplication)).Methods("POST")
+	router.HandleFunc("/application/{appName}", handler.InsertNewNamespace).Methods("POST")
 	router.HandleFunc("/application/{app}/namespaces/{namespaces}/configurations",WithAuth(handler.GetLatestConfiguration)).Methods("GET")
 	router.HandleFunc("/login", handler.LoginHandler).Methods("POST")
 	return router
