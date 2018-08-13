52a29fb74d2115c4d71f27094a1d7d2aa4f178a5
Andri Suranta Ginting <andris.ginting@gmail.com>
2018-08-13 18:30:43 +0700

    add new grant privilege

--- migrations/1533634183_roles.up.sql
+++ migrations/1533634183_roles.up.sql
@@ -2,6 +2,7 @@ CREATE ROLE admin;
 CREATE ROLE client;
 
 GRANT INSERT,SELECT,UPDATE ON configuration,namespace,application TO admin;
+GRANT INSERT,SELECT,UPDATE ON configuration,configuration_change,history TO admin;
 GRANT USAGE, SELECT ON SEQUENCE application_id_seq,history_id_seq, namespace_id_seq, users_id_seq TO admin;
 
 GRANT SELECT ON configuration,application,namespace,users TO client;
